a6b0d4b78dc7ed078e71c9f9d247d832673efe42
brianngeywo <brianngeywo254@gmail.com>
2020-05-21 01:21:08 +0300

    root and login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  root to: "pages#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "chatroom#index"
+  resources :users, except: [:new]
+  get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
