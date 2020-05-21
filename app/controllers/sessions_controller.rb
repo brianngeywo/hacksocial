659e7fd76907038b98537ffe0121f99bc69728c8
brianngeywo <brianngeywo254@gmail.com>
2020-05-21 14:02:08 +0300

    finished rendering messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+    before_action :login_redirect, only: [:new,:create]
     def new
     end
     def create
@@ -6,7 +7,7 @@ class SessionsController < ApplicationController
         if user && user.authenticate(params[:session][:password])
             session[:user_id] = user.id
            flash[:success] = "Login successfull"
-           redirect_to user_path(user)
+           redirect_to root_path
         else
             flash.now[:danger] = "There was something wrong with the login information"
             render 'new'
@@ -15,6 +16,14 @@ class SessionsController < ApplicationController
     def destroy
         session[:user_id] = nil
         flash[:success] = "logout successfull"
-        redirect_to root_path
+        render 'new'
+    end
+
+    private
+    def login_redirect
+        if logged_in?
+            flash[:error] = "you are already logged in"
+            redirect_to root_path
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root to: "chatroom#index"
-  resources :messages
   resources :users, except: [:new]
   get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  post 'message', to: 'messages#create'
 end
